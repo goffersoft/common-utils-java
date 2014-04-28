1b1344691d3a492c4815ba907aac339ba279a16a
goffer123 <goffersoft@gmail.com>
2014-04-28 19:10:06 -0400

    Initial Checkin

---
+++ src/test/java/com/goffersoft/common/utils/BitUtilsTest.java
@@ -0,0 +1,164 @@
+package com.goffersoft.common.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BitUtilsTest {
+
+	@Test
+	public void testIsBitSetLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBitSetIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBitSetShortInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBitSetByteInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumBitsSetLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumBitsSetLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumBitsSetInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumBitsSetShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumBitsSetByte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBitIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBitShortInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBitByteInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearBitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearBitIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearBitShortInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearBitByteInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlipBitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlipBitIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlipBitShortInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlipBitByteInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBitPosLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBitPosLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBitPosInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBitPosShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBitPosByte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBitmapIteratorLongIntBitmapIteratorObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBitmapIteratorLongBitmapIteratorObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBitmapIteratorIntBitmapIteratorObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBitmapIteratorShortBitmapIteratorObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBitmapIteratorByteBitmapIteratorObject() {
+		fail("Not yet implemented");
+	}
+
+}
