afbda6be7ea8a85b2f2b4c706ab4c521e4b22c8c
goffer123 <goffersoft@gmail.com>
2014-04-28 19:03:34 -0400

    initial checkin

---
+++ src/test/java/com/goffersoft/common/utils/EndianConversionTest.java
@@ -0,0 +1,189 @@
+package com.goffersoft.common.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EndianConversionTest {
+
+	@Test
+	public void testIntToByteArrayLEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntToByteArrayLEByteArrayIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntToOutputStreamLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToIntLEByteArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToIntLEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToIntLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToByteArrayLEByteArrayIntShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToByteArrayLEByteArrayIntIntShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToOutputStreamLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToShortLEByteArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToShortLEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToShortLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToByteArrayLEByteArrayIntLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToByteArrayLEByteArrayIntIntLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToOutputStreamLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToLongLEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToLongLEByteArrayIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToLongLE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntToByteArrayBEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntToByteArrayBEByteArrayIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIntToOutputStreamBE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToIntBEByteArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToIntBEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToIntBE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToByteArrayBEByteArrayIntShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToByteArrayBEByteArrayIntIntShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShortToOutputStreamBE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToShortBEByteArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToShortBEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToShortBE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToByteArrayBEByteArrayIntLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToByteArrayBEByteArrayIntIntLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLongToOutputStreamBE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToLongBEByteArrayIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testByteArrayToLongBEByteArrayIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputStreamToLongBE() {
+		fail("Not yet implemented");
+	}
+
+}
